dc30b57a472f15600b40a195d7ebc99d8d4ce140
vaibhav <bomlevaihav@gmail.com>
2024-09-17 09:10:23 +0530

    Imported library created chat start method to get a file when user uploaded,split it using taxt splitter

--- app.py
+++ app.py
@@ -19,4 +19,29 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 print(OPENAI_API_KEY)
 
+text_splitter = RecursiveCharacterTextSplitter(chunk_sie=1000,chunk_overlap=100)
+
+@cl.on_chat_start
+async def on_chat_start():
+    files = None
+
+    # Wait for user to upload the file
+    while files == None:
+        files = await cl.AskFileMessage(
+            content = "Please upload a text file to begin!",
+            accept = ["text/plain"],
+            max_size_mb= 20,
+            timeout=180
+        ).send()
+    
+    file = files[0]
+
+    msg = cl.Message(content=f"Processing `{file.name}`...", disable_feedback=True)
+    await msg.send()
+
+    with open(file.path, "r",encoding="utf-8") as f:
+        text = f.read()
+
+    # Split the text into chaunks
+    
 
