977aa3a4c78bd531fd8da1fbaf7299ce259c86f6
vaibhav <bomlevaihav@gmail.com>
2024-09-17 08:57:34 +0530

    resolve issue added langchain_community library

--- app.py
+++ app.py
@@ -11,5 +11,12 @@ from langchain.docstore.document import Document
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
 
 import chainlit as cl
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+print(OPENAI_API_KEY)
 
 
